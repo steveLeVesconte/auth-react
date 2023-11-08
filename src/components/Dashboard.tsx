6a2f35618e88308d55616a198308aa9dcaba24fc
steveLeVesconte <SteveLeVesconte@Gmail.com>
2023-11-08 04:58:26 -0800

    remove extra import from Dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { Alert, Button, Card} from 'react-bootstrap'
 import {useAuth} from '../contexts/AuthContext'
 import { Link, useNavigate } from 'react-router-dom';
